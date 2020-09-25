8489935dcd0060c976ce23aee8670698e402365e
40823217 <40823217@gm.nfu.edu.tw>
2020-09-25 13:34:15 +0800

    add shh and test upload

--- init.py
+++ init.py
@@ -10,7 +10,7 @@ config_dir = _curdir + "/config/"
 class Init(object):
     # uwsgi as static class variable, can be accessed by Init.uwsgi
     uwsgi = False
-    site_title = "CMSiMDE"
+    site_title = "40823217 cad2020"
     def __init__(self):
         # hope to create downloads and images directories　
         if not os.path.isdir(_curdir + "/downloads"):
